53b3b0e6f9d31f215341cfe432791d7ada183ab2
Josh Zamor <josh.zamor@villagereach.org>
2016-10-07 00:44:50 -0700

    add reference data service to doc collection

--- docs/source/collect-docs.py
+++ docs/source/collect-docs.py
@@ -2,8 +2,8 @@ import urllib
 
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-requisition/master/README.md", "developer-docs/requisition.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-auth/master/README.md", "developer-docs/auth.md")
+urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-referencedata/master/README.md", "developer-docs/referencedata.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/README.md", "developer-docs/templateService.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-requisition-refUI/master/README.md", "developer-docs/requisitionUI.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/STYLE-GUIDE.md", "developer-docs/codeStyleguide.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/TESTING.md", "developer-docs/testing.md")
-
