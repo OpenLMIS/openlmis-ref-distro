f2f1a20e8a104a29a9138f40dd3045d9390add29
Brandon Bowersox-Johnson <brandonbowersox@gmail.com>
2016-11-21 11:41:21 -0800

    Adding fulfillment service demo-data to blue script

--- utils/demo-data.sh
+++ utils/demo-data.sh
@@ -22,14 +22,18 @@ if [ "$UPDATE" = true ] ; then
    git pull
    cd $OPENLMIS_HOME/openlmis-requisition
    git pull
+   cd $OPENLMIS_HOME/openlmis-fulfillment
+   git pull
    cd $OPENLMIS_HOME/openlmis-auth
    git pull
 
    sudo docker-compose -f $OPENLMIS_HOME/openlmis-referencedata/docker-compose.builder.yml run demo-data
    sudo docker-compose -f $OPENLMIS_HOME/openlmis-requisition/docker-compose.builder.yml run demo-data
+   sudo docker-compose -f $OPENLMIS_HOME/openlmis-fulfillment/docker-compose.builder.yml run demo-data
    sudo docker-compose -f $OPENLMIS_HOME/openlmis-auth/docker-compose.builder.yml run demo-data
 fi
 
 sudo docker exec -i openlmisblue_db_1 psql -Upostgres open_lmis < $OPENLMIS_HOME/openlmis-referencedata/build/demo-data/data.sql
 sudo docker exec -i openlmisblue_db_1 psql -Upostgres open_lmis < $OPENLMIS_HOME/openlmis-auth/build/demo-data/data.sql
 sudo docker exec -i openlmisblue_db_1 psql -Upostgres open_lmis < $OPENLMIS_HOME/openlmis-requisition/build/demo-data/data.sql
+sudo docker exec -i openlmisblue_db_1 psql -Upostgres open_lmis < $OPENLMIS_HOME/openlmis-fulfillment/build/demo-data/data.sql
