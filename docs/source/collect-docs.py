08ba588b26608607e623732b55bb181cb8221b0c
Josh Zamor <josh.zamor@villagereach.org>
2017-01-12 19:06:00 -0800

    add sourcing of error handling doc

--- docs/source/collect-docs.py
+++ docs/source/collect-docs.py
@@ -8,3 +8,4 @@ urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-requisition-refUI/master/README.md", "developer-docs/requisitionUI.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/STYLE-GUIDE.md", "developer-docs/codeStyleguide.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/TESTING.md", "developer-docs/testing.md")
+urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/ERROR_HANDLING.md", "developer-docs/errorHandling.md")
