6df986deb14f9f478466c0cdb944932f3c3d5fac
Nick Reid <nickreid@nickreid.com>
2017-03-30 14:23:19 -0700

    Updated like to reference-ui documentation

--- docs/source/collect-docs.py
+++ docs/source/collect-docs.py
@@ -6,7 +6,7 @@ urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-auth/mas
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-referencedata/master/README.md", "developer-docs/referencedata.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-stockmanagement/master/README.md", "developer-docs/stockmanagement.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/README.md", "developer-docs/templateService.md")
-urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-requisition-refUI/master/README.md", "developer-docs/requisitionUI.md")
+urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-reference-ui/master/README.md", "developer-docs/refereceUI.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/STYLE-GUIDE.md", "developer-docs/codeStyleguide.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/TESTING.md", "developer-docs/testing.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/ERROR_HANDLING.md", "developer-docs/errorHandling.md")
