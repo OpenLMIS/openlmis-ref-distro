c6a803b1ee35143d3e7cb34ee3f2c752e4287873
Josh Zamor <josh.zamor@villagereach.org>
2017-03-30 15:26:36 -0700

    fixing reference ui doc name

--- docs/source/collect-docs.py
+++ docs/source/collect-docs.py
@@ -5,7 +5,7 @@ urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-requisit
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-auth/v3.0.1/README.md", "developer-docs/auth.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-referencedata/v4.0.0/README.md", "developer-docs/referencedata.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/README.md", "developer-docs/templateService.md")
-urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-reference-ui/master/README.md", "developer-docs/refereceUI.md")
+urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-reference-ui/master/README.md", "developer-docs/referenceUI.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/STYLE-GUIDE.md", "developer-docs/codeStyleguide.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/TESTING.md", "developer-docs/testing.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/ERROR_HANDLING.md", "developer-docs/errorHandling.md")
