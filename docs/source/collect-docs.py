af7253294d2ebe013cd4a41ae7887f74739188cd
Josh Zamor <josh.zamor@villagereach.org>
2017-05-08 15:37:58 +0200

    fix version typo

--- docs/source/collect-docs.py
+++ docs/source/collect-docs.py
@@ -2,7 +2,7 @@ import urllib
 
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-fulfillment/v3.1.0/README.md", "developer-docs/fulfillment.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-requisition/v3.1.2/README.md", "developer-docs/requisition.md")
-urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-auth/3.0.2/README.md", "developer-docs/auth.md")
+urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-auth/v3.0.2/README.md", "developer-docs/auth.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-referencedata/v5.0.0/README.md", "developer-docs/referencedata.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/README.md", "developer-docs/templateService.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-reference-ui/v5.0.0/README.md", "developer-docs/referenceUI.md")
