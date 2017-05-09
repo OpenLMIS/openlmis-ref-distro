6ec684e7d545f4823bafcff54c894cb843c95e79
Josh Zamor <josh.zamor@villagereach.org>
2017-05-09 17:56:12 +0200

    move referencedata readme doc to 5.0.1

--- docs/source/collect-docs.py
+++ docs/source/collect-docs.py
@@ -3,7 +3,7 @@ import urllib
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-fulfillment/v3.0.2/README.md", "developer-docs/fulfillment.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-requisition/v3.1.2/README.md", "developer-docs/requisition.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-auth/v3.0.2/README.md", "developer-docs/auth.md")
-urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-referencedata/v5.0.0/README.md", "developer-docs/referencedata.md")
+urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-referencedata/v5.0.1/README.md", "developer-docs/referencedata.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/README.md", "developer-docs/templateService.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-reference-ui/v5.0.0/README.md", "developer-docs/referenceUI.md")
 urllib.urlretrieve("https://raw.githubusercontent.com/OpenLMIS/openlmis-template-service/master/STYLE-GUIDE.md", "developer-docs/codeStyleguide.md")
