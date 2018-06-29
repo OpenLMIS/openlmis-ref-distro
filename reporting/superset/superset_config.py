fdd856ae0ac7b4d4273f93fd006ba05a39ab14d2
Dickson Ukang'a <ukanga@gmail.com>
2018-06-29 20:46:47 +0300

    Create superset DB in db service

--- reporting/superset/superset_config.py
+++ reporting/superset/superset_config.py
@@ -1,4 +1,4 @@
 SQLALCHEMY_DATABASE_URI = \
-    'postgresql+psycopg2://postgres:p@ssw0rd@db:5432/superset'
+        'postgresql+psycopg2://superset:superset123@db:5432/superset'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'thisISaSECRET_1234'
