74f02322ae58577c1ade0cb8719ed6b1e4d00674
nateMJ <natemaritim@gmail.com>
2018-08-31 15:31:53 +0300

    Update create tables statements

--- reporting/db/docker-entrypoint-initdb.d/templates/referenceDataCreateTableStatements.sql
+++ reporting/db/docker-entrypoint-initdb.d/templates/referenceDataCreateTableStatements.sql
@@ -1,7 +1,7 @@
 
 -- Referencedata create table statements
 -- Created by Craig Appl (cappl@ona.io)
--- Last Updated 14 August 2018
+-- Last Updated 31 August 2018
 --
 
 --
@@ -38,22 +38,23 @@ ALTER TABLE commodity_types OWNER TO postgres;
 
 CREATE TABLE facilities (
     id uuid NOT NULL UNIQUE,
-    status boolean,
     code character varying(255),
-    comment text,
-    description text,
-    enabled boolean,
-    godowndate date,
-    golivedate date,
     name character varying(255),
-    openlmisaccessible boolean,
+    status boolean,
+    enabled boolean,
+    type character varying(255),
+    operator_code character varying(255),
+    operator_name character varying(255),
+    operator_id character varying(255),
     district character varying(255),
     region character varying(255),
     country character varying(255),
-    operator_code character varying(255),
-    operator_name character varying(255),
-    operator_id uuid,
-    type character varying(255),
+    openlmisaccessible character varying(255),
+    golivedate date,
+    godowndate date,
+    openlmisaccessible boolean,
+    comment text,
+    description text,
     extradata jsonb,
     location public.geometry
 );
@@ -62,21 +63,19 @@ CREATE TABLE facilities (
 ALTER TABLE facilities OWNER TO postgres;
 
 --
--- Name: facility_type_approved_products; Type: TABLE; Schema: referencedata; Owner: postgres
+-- Name: facility_operators; Type: TABLE; Schema: referencedata; Owner: postgres
 --
 
-CREATE TABLE facility_type_approved_products (
+CREATE TABLE facility_operators (
     id uuid NOT NULL UNIQUE,
-    emergencyorderpoint double precision,
-    maxperiodsofstock double precision NOT NULL,
-    minperiodsofstock double precision,
-    facilitytypeid uuid NOT NULL,
-    orderableid uuid NOT NULL,
-    programid uuid NOT NULL
+    code text,
+    description text,
+    displayorder integer,
+    name character varying(255)
 );
 
 
-ALTER TABLE facility_type_approved_products OWNER TO postgres;
+ALTER TABLE facility_operators OWNER TO postgres;
 
 --
 -- Name: ideal_stock_amounts; Type: TABLE; Schema: referencedata; Owner: postgres
@@ -84,10 +83,10 @@ ALTER TABLE facility_type_approved_products OWNER TO postgres;
 
 CREATE TABLE ideal_stock_amounts (
     id uuid NOT NULL UNIQUE,
-    facilityid uuid NOT NULL,
-    processingperiodid uuid NOT NULL,
+    facilityid uuid,
+    processingperiodid uuid,
     amount integer,
-    commoditytypeid uuid NOT NULL
+    commoditytypeid uuid
 );
 
 
@@ -99,11 +98,11 @@ ALTER TABLE ideal_stock_amounts OWNER TO postgres;
 
 CREATE TABLE lots (
     id uuid NOT NULL UNIQUE,
-    lotcode text NOT NULL,
+    lotcode character varying(255),
     expirationdate date,
     manufacturedate date,
-    tradeitemid uuid NOT NULL,
-    active boolean NOT NULL
+    tradeitemid uuid,
+    active boolean
 );
 
 
@@ -115,24 +114,23 @@ ALTER TABLE lots OWNER TO postgres;
 
 CREATE TABLE orderables (
     id uuid NOT NULL UNIQUE,
+    code character varying(255),
     fullproductname character varying(255),
     packroundingthreshold bigint,
     netcontent bigint,
-    code character varying(255),
     roundtozero boolean,
     description character varying(255),
-    extradata jsonb,
-    dispensableid uuid,
     programid character varying(255),
     orderabledisplaydategoryid character varying(255),
     orderablecategorydisplayname character varying(255),
-    orderablecategorydisplayorder character varying(255),
+    orderablecategorydisplayorder int,
     active boolean,
     fullsupply boolean,
     displayorder int,
     dosesperpatient int,
     priceperpack double precision,
-    tradeitemid character varying(255)
+    tradeitemid character varying(255),
+    extradata jsonb
 );
 
 
@@ -145,17 +143,33 @@ ALTER TABLE orderables OWNER TO postgres;
 CREATE TABLE processing_periods (
     id uuid NOT NULL UNIQUE,
     description text,
-    enddate date NOT NULL,
-    name text NOT NULL,
-    startdate date NOT NULL,
-    durationinmonths int,
-    processingscheduleid uuid NOT NULL,
-    processingschedulename character varying(255),
-    processingschedulecode character varying(255)
+    enddate date,
+    name character varying(255),
+    startdate date,
+    processingscheduleid uuid
 );
 
 
 ALTER TABLE processing_periods OWNER TO postgres;
+
+--
+-- Name: program_orderables; Type: TABLE; Schema: referencedata; Owner: postgres
+--
+
+CREATE TABLE program_orderables (
+    id uuid NOT NULL UNIQUE,
+    active boolean,
+    displayorder integer,
+    dosesperpatient integer,
+    fullsupply boolean,
+    priceperpack numeric(19,2),
+    orderabledisplaycategoryid uuid,
+    orderableid uuid,
+    programid uuid
+);
+
+
+ALTER TABLE program_orderables OWNER TO postgres;
 
 --
 -- Name: programs; Type: TABLE; Schema: referencedata; Owner: postgres
@@ -182,7 +196,7 @@ ALTER TABLE programs OWNER TO postgres;
 
 CREATE TABLE requisition_group_members (
     requisitiongroupid uuid NOT NULL,
-    facilityid uuid NOT NULL
+    facilityid uuid
 );
 
 
@@ -194,77 +208,15 @@ ALTER TABLE requisition_group_members OWNER TO postgres;
 
 CREATE TABLE requisition_group_program_schedules (
     id uuid NOT NULL UNIQUE,
-    directdelivery boolean NOT NULL,
+    directdelivery boolean,
     dropofffacilityid uuid,
-    processingscheduleid uuid NOT NULL,
-    programid uuid NOT NULL,
-    requisitiongroupid uuid NOT NULL
+    processingscheduleid uuid,
+    programid uuid,
+    requisitiongroupid uuid
 );
 
 
 ALTER TABLE requisition_group_program_schedules OWNER TO postgres;
-
---
--- Name: requisition_groups; Type: TABLE; Schema: referencedata; Owner: postgres
---
-
-CREATE TABLE requisition_groups (
-    id uuid NOT NULL UNIQUE,
-    code text NOT NULL,
-    description text,
-    name text NOT NULL,
-    supervisorynodeid uuid NOT NULL
-);
-
-
-ALTER TABLE requisition_groups OWNER TO postgres;
-
---
--- Name: stock_adjustment_reasons; Type: TABLE; Schema: referencedata; Owner: postgres
---
-
-CREATE TABLE stock_adjustment_reasons (
-    id uuid NOT NULL UNIQUE,
-    additive boolean,
-    description text,
-    displayorder integer,
-    name text NOT NULL,
-    programid uuid NOT NULL
-);
-
-
-ALTER TABLE stock_adjustment_reasons OWNER TO postgres;
-
---
--- Name: supervisory_nodes; Type: TABLE; Schema: referencedata; Owner: postgres
---
-
-CREATE TABLE supervisory_nodes (
-    id uuid NOT NULL UNIQUE,
-    code text NOT NULL,
-    description text,
-    name text NOT NULL,
-    facilityid uuid,
-    parentid uuid
-);
-
-
-ALTER TABLE supervisory_nodes OWNER TO postgres;
-
---
--- Name: supply_lines; Type: TABLE; Schema: referencedata; Owner: postgres
---
-
-CREATE TABLE supply_lines (
-    id uuid NOT NULL UNIQUE,
-    description text,
-    programid uuid NOT NULL,
-    supervisorynodeid uuid NOT NULL,
-    supplyingfacilityid uuid NOT NULL
-);
-
-
-ALTER TABLE supply_lines OWNER TO postgres;
 
 --
 -- Name: supported_programs; Type: TABLE; Schema: referencedata; Owner: postgres
@@ -273,9 +225,9 @@ ALTER TABLE supply_lines OWNER TO postgres;
 CREATE TABLE supported_programs (
     active boolean NOT NULL,
     startdate date,
-    facilityid uuid NOT NULL,
-    programid uuid NOT NULL,
-    locallyfulfilled boolean DEFAULT false NOT NULL
+    facilityid uuid,
+    programid uuid,
+    locallyfulfilled boolean DEFAULT false
 );
 
 
@@ -285,12 +237,128 @@ ALTER TABLE supported_programs OWNER TO postgres;
 -- Name: trade_item_classifications; Type: TABLE; Schema: referencedata; Owner: postgres
 --
 
-CREATE TABLE trade_items (
+CREATE TABLE trade_item_classifications (
     id uuid NOT NULL UNIQUE,
     classificationsystem character varying(255),
     classificationid character varying(255),
-    manufactureroftradeitem character varying(255)
+    tradeitemid uuid
+);
+
+
+ALTER TABLE trade_item_classifications OWNER TO postgres;
+
+--
+-- Name: trade_items; Type: TABLE; Schema: referencedata; Owner: postgres
+--
+
+CREATE TABLE trade_items (
+    id uuid NOT NULL UNIQUE,
+    manufactureroftradeitem character varying(255),
+    gtin text
 );
 
 
 ALTER TABLE trade_items OWNER TO postgres;
+
+--
+-- Name: rights; Type: TABLE; Schema: referencedata; Owner: postgres
+--
+
+CREATE TABLE rights (
+    id uuid NOT NULL UNIQUE,
+    name character varying(255),
+    type character varying(255)
+);
+
+
+ALTER TABLE rights OWNER TO postgres;
+
+--
+-- Name: users; Type: TABLE; Schema: referencedata; Owner: postgres
+--
+
+CREATE TABLE users (
+    id uuid NOT NULL UNIQUE,
+    username character varying(255),
+    firstname character varying(255),
+    lastname character varying(255),
+    homefacilityid uuid,
+    active boolean,
+    loginRestricted boolean
+);
+
+
+ALTER TABLE users OWNER TO postgres;
+
+--
+-- Name: roles; Type: TABLE; Schema: referencedata; Owner: postgres
+--
+
+CREATE TABLE roles (
+    id uuid NOT NULL UNIQUE,
+    name character varying(255),
+    description character varying(255),
+    rightsname character varying(255),
+    rightsid uuid,
+    rightstype character varying(255),
+    count INT
+);
+
+
+ALTER TABLE roles OWNER TO postgres;
+
+--
+-- Name: supervisorynodes; Type: TABLE; Schema: referencedata; Owner: postgres
+--
+
+CREATE TABLE supervisorynodes (
+    id uuid NOT NULL UNIQUE,
+    name character varying(255),
+    code character varying(255),
+    facilityname character varying(255),
+    facilityid uuid,
+    requisitiongroupname character varying(255),
+    requisitiongroupid uuid,
+    parentnodename character varying(255),
+    parentnodeid uuid
+);
+
+
+ALTER TABLE supervisorynodes OWNER TO postgres;
+
+--
+-- Name: requisitiongroups; Type: TABLE; Schema: referencedata; Owner: postgres
+--
+
+CREATE TABLE requisitiongroups (
+    id uuid NOT NULL UNIQUE,
+    name character varying(255),
+    code character varying(255),
+    facilityid uuid,
+    supervisorynodeid uuid,
+    supervisorynodename character varying(255),
+    supervisorynodecode character varying(255),
+    programname character varying(255),
+    programid uuid,
+    processingscheduleid uuid,
+    directdelivery boolean,
+    memberfacilityid jsonb
+);
+
+
+ALTER TABLE requisitiongroups OWNER TO postgres;
+
+--
+-- Name: supplylines; Type: TABLE; Schema: referencedata; Owner: postgres
+--
+
+CREATE TABLE supplylines (
+    id uuid NOT NULL UNIQUE,
+    description character varying(255),
+    supervisorynodeid uuid,
+    programid uuid,
+    supplyingfacilityid uuid
+);
+
+
+ALTER TABLE supplylines OWNER TO postgres;
