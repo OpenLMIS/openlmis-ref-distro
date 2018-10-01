f4a7f9d2f69db026c71fd4747a1a43e675a3154b
nateMJ <natemaritim@gmail.com>
2018-10-01 15:08:55 +0300

    WIP: Add custom oauth sign in

--- reporting/config/services/superset/superset_config.py
+++ reporting/config/services/superset/superset_config.py
@@ -1,4 +1,21 @@
+from flask_appbuilder.security.manager import AUTH_OID, AUTH_REMOTE_USER, AUTH_DB, AUTH_LDAP, AUTH_OAUTH
 SQLALCHEMY_DATABASE_URI = \
     'postgresql+psycopg2://superset:superset123@db:5432/superset'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'thisISaSECRET_1234'
+
+AUTH_TYPE = AUTH_OAUTH
+
+OAUTH_PROVIDERS = [
+    {'name': 'openlmis', 'icon': 'fa-google',
+        'remote_app': {
+            'consumer_key': 'tableau-wdc',
+            'consumer_secret': 'changeme',
+            'base_url': 'https://uat.openlmis.org/api/oauth',
+            'access_token_url': 'https://uat.openlmis.org/api/oauth/token',
+            'authorize_url': 'https://uat.openlmis.org/api/oauth/authorize?response_type=token&client_id=tableau-wdc&scope=read&redirect_uri=http://localhost:8088/superset/welcome'}
+     }
+]
+
+# The default user self registration role
+AUTH_USER_REGISTRATION_ROLE = "Admin"
