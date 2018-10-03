e1db80a9e29027be7decf3910a82e6f2fbad7801
nateMJ <natemaritim@gmail.com>
2018-10-03 16:59:59 +0300

    Add user registration config

--- reporting/config/services/superset/superset_config.py
+++ reporting/config/services/superset/superset_config.py
@@ -7,15 +7,18 @@ SECRET_KEY = 'thisISaSECRET_1234'
 AUTH_TYPE = AUTH_OAUTH
 
 OAUTH_PROVIDERS = [
-    {'name': 'openlmis', 'icon': 'fa-google',
+    {'name': 'openlmis', 'icon': 'fa-google', 'token_key': 'access_token',
         'remote_app': {
             'consumer_key': 'tableau-wdc',
             'consumer_secret': 'changeme',
             'base_url': 'https://uat.openlmis.org/api/oauth',
             'access_token_url': 'https://uat.openlmis.org/api/oauth/token',
-            'authorize_url': 'https://uat.openlmis.org/api/oauth/authorize?response_type=token&client_id=tableau-wdc&scope=read&redirect_uri=http://localhost:8088/superset/welcome'}
+            'authorize_url': 'https://uat.openlmis.org/api/oauth/authorize?response_type=token&redirect_uri=http://localhost:8088/superset/welcome'}
      }
 ]
 
 # The default user self registration role
 AUTH_USER_REGISTRATION_ROLE = "Admin"
+
+# Will allow user self registration
+AUTH_USER_REGISTRATION = True
