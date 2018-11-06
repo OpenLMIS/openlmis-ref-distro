1f5b2d4fcbe3f22691b0d8de1074019a259562c7
Wambere <janette@wambere.com>
2018-11-06 15:21:05 +0300

    Get user information from openlmis to create account in superset during auth

--- reporting/config/services/superset/superset_config.py
+++ reporting/config/services/superset/superset_config.py
@@ -15,14 +15,14 @@ OAUTH_PROVIDERS = [
         'icon': 'fa-google',
         'token_key':'access_token',
         'remote_app': {
-            'consumer_key': 'tableau-wdc',
+            'consumer_key': 'superset',
             'consumer_secret': 'changeme',
             'request_token_params': {
                 'scope': 'read write'
             },
             'access_token_method': 'POST',
             'access_token_headers': {
-                'Authorization':'Basic dGFibGVhdS13ZGM6Y2hhbmdlbWU='
+                'Authorization':'Basic c3VwZXJzZXQ6Y2hhbmdlbWU='
             },
             'base_url': 'https://uat.openlmis.org/api/oauth',
             'access_token_url': 'https://uat.openlmis.org/api/oauth/token?grant_type=authorization_code',
@@ -31,7 +31,7 @@ OAUTH_PROVIDERS = [
 ]
 
 # The default user self registration role
-AUTH_USER_REGISTRATION_ROLE = "Admin"
+AUTH_USER_REGISTRATION_ROLE = "Public"
 
 # Will allow user self registration
 AUTH_USER_REGISTRATION = True
@@ -39,19 +39,22 @@ AUTH_USER_REGISTRATION = True
 from superset.security import SupersetSecurityManager
 import logging
 class CustomSecurityManager(SupersetSecurityManager):
-    # def __init__(self, appbuilder):
-    #     super(CustomSecurityManager, self).__init__(appbuilder)
-    #     logging.info('testing init')
-    #     self.oauth_user_info = self.soauth_user_info
-    #     # self.oauth_user_info = self.oauth_user_info(self, provider=None, response=None)
-    
-    def oauth_user_info(self, provider, response=None):
-        if provider == 'openlmis':
-            me = self.appbuilder.sm.oauth_remotes[provider].get(
-                'userDetails').data
-            logging.info(me)
-            logging.debug("user_data: {0}".format(me))
-            return {'name': me['user_name'], 'email': 'test@openlmis.org', 'id': me['username'], 'username': me['user_name'], 'first_name': '', 'last_name': ''}
 
+    def oauth_user_info(self, provider, response=None):
+        logging.debug("Oauth2 provider: {0}.".format(provider))
+        if provider == 'openlmis':
+            # get access token
+            my_token = self.oauth_tokengetter()[0]
+            # get referenceDataUserId
+            reference_user_id = self.appbuilder.sm.oauth_remotes[provider].get('oauth/check_token', data={'token': my_token})
+            reference_data_user_id = reference_user_id.data['referenceDataUserId']
+            # get user details
+            endpoint = 'users/{}'.format(reference_data_user_id)
+            user_info = self.appbuilder.sm.oauth_remotes[provider].get(endpoint)
+            me = user_info.data
+            # get email
+            email_endpoint = 'userContactDetails/{}'.format(reference_data_user_id)
+            email = self.appbuilder.sm.oauth_remotes[provider].get(email_endpoint)
+            return {'name': me['username'], 'email': email.data['emailDetails']['email'], 'id': me['id'], 'username': me['username'], 'first_name': me['firstName'], 'last_name': me['lastName']}
 
 CUSTOM_SECURITY_MANAGER = CustomSecurityManager
