73c6f6c7b463c22fc9248b0489dad0dd917f5cc6
Craig Appl <cappl@ona.io>
2018-11-13 18:18:04 +0000

    Revert "OLMIS-5692 Changing Consul name to reflect SUPERSET_DOMAIN_NAME to fix redirect_uri in oAuth process"
    This reverts commit e1441d590eabf20fda60db7e9f0d5997cf092d34.

--- reporting/config/services/superset/superset_config.py
+++ reporting/config/services/superset/superset_config.py
@@ -37,9 +37,7 @@ AUTH_USER_REGISTRATION_ROLE = "Public"
 AUTH_USER_REGISTRATION = True
 
 # Allow iFrame access from openLMIS running on localhost
-HTTP_HEADERS = {
-    'X-Frame-Options': 'allow-from https://uat.openlmis.org'
-     }
+HTTP_HEADERS = {'X-Frame-Options': 'allow-from https://uat.openlmis.org'}
 
 from superset.security import SupersetSecurityManager
 import logging
