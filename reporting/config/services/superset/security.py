4fd650d696d430d1ced07f79db8b95e6c94f1720
Craig Appl <cappl@ona.io>
2018-11-16 19:09:54 +0000

    OLMIS-5718 Upgrade Superset to 0.28.1 and OLMIS-5690 Fixed missing redirect

--- reporting/config/services/superset/security.py
+++ reporting/config/services/superset/security.py
@@ -5,6 +5,7 @@
 from flask import flash, redirect, session, url_for, g, request
 from flask_appbuilder.security.views import expose, AuthOAuthView
 from superset.security import SupersetSecurityManager
+from flask_login import login_user
 import logging
 class AuthOAuthView(AuthOAuthView):
     login_template = 'appbuilder/general/security/login_oauth.html'
@@ -15,11 +16,13 @@ class AuthOAuthView(AuthOAuthView):
     def login(self, provider=None, register=None):
         logging.debug('Provider: {0}'.format(provider))
         #Set the redirect_url
-        redirect_url = self.appbuilder.get_url_for_index
-        if request.args.get('redirect_url') is not None:
-            redirect_url = request.args.get('redirect_url')
+        redirect_url = request.args.get('redirect_url')
+        if redirect_url is None:
+        	redirect_url = self.appbuilder.get_url_for_index
 
-        if g.user is not None and g.user.is_authenticated and not g.user.is_anonymous():
+        #logging.debug("redirect_url: "+ redirect_url)
+
+        if g.user is not None and g.user.is_authenticated and not g.user.is_anonymous:
             logging.debug("Already authenticated {0}".format(g.user))
             return redirect(redirect_url)
         if provider is None:
@@ -33,11 +36,51 @@ class AuthOAuthView(AuthOAuthView):
                 if register:
                     logging.debug('Login to Register')
                     session['register'] = True
-                return self.appbuilder.sm.oauth_remotes[provider].authorize(callback=url_for('.oauth_authorized',provider=provider, _external=True))
+                return self.appbuilder.sm.oauth_remotes[provider].authorize(callback=url_for('.oauth_authorized',provider=provider, _external=True, redirect_url=redirect_url))
             except Exception as e:
                 logging.error("Error on OAuth authorize: {0}".format(e))
                 flash(as_unicode(self.invalid_login_message), 'warning')
                 return redirect(self.appbuilder.get_url_for_index)
+
+    @expose('/oauth-authorized/<provider>')
+    def oauth_authorized(self, provider):
+        logging.debug("Authorized init")
+        resp = self.appbuilder.sm.oauth_remotes[provider].authorized_response()
+        redirect_url = request.args.get('redirect_url')
+        if resp is None:
+            flash(u'You denied the request to sign in.', 'warning')
+            return redirect('login')
+        logging.debug('OAUTH Authorized resp: {0}'.format(resp))
+        # Retrieves specific user info from the provider
+        try:
+            self.appbuilder.sm.set_oauth_session(provider, resp)
+            userinfo = self.appbuilder.sm.oauth_user_info(provider, resp)
+        except Exception as e:
+            logging.error("Error returning OAuth user info: {0}".format(e))
+            user = None
+        else:
+            logging.debug("User info retrieved from {0}: {1}".format(provider, userinfo))
+            # User email is not whitelisted
+            if provider in self.appbuilder.sm.oauth_whitelists:
+                whitelist = self.appbuilder.sm.oauth_whitelists[provider]
+                allow = False
+                for e in whitelist:
+                    if re.search(e, userinfo['email']):
+                        allow = True
+                        break
+                if not allow:
+                    flash(u'You are not authorized.', 'warning')
+                    return redirect('login')
+            else:
+                logging.debug('No whitelist for OAuth provider')
+            user = self.appbuilder.sm.auth_user_oauth(userinfo)
+
+        if user is None:
+            flash(as_unicode(self.invalid_login_message), 'warning')
+            return redirect('login')
+        else:
+            login_user(user)
+            return redirect(redirect_url)
 
 class CustomSecurityManager(SupersetSecurityManager):
 
