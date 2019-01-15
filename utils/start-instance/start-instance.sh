cc1aea45e86d9ee2f20b904f6ef6e7994ab284e3
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-01-15 17:09:49 +0100

    OLMIS-5728: fixed aws cli command in start instances script

--- utils/start-instance/start-instance.sh
+++ utils/start-instance/start-instance.sh
@@ -5,7 +5,7 @@
 start_instance() {
   # starting instance
 	echo "Stopping instance $TARGET_INSTANCE_ID"
-	aws ec2 stop-instances --instance-ids $TARGET_INSTANCE_ID
+	aws ec2 start-instances --instance-ids $TARGET_INSTANCE_ID
 
   echo "Waiting for $TARGET_INSTANCE_ID to be available"
   aws ec2 instance-status-ok --instance-ids $TARGET_INSTANCE_ID
