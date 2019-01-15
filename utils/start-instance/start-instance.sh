bf619670157f3435456ee7a5e5db39b1e7955c38
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-01-15 19:31:29 +0100

    OLMIS-5728: fixed waiting for instance part of starting script

--- utils/start-instance/start-instance.sh
+++ utils/start-instance/start-instance.sh
@@ -7,8 +7,31 @@ start_instance() {
 	echo "Starting instance $TARGET_INSTANCE_ID"
 	aws ec2 start-instances --instance-ids $TARGET_INSTANCE_ID
 
-  echo "Waiting for $TARGET_INSTANCE_ID to be available"
-  aws ec2 instance-status-ok --instance-ids $TARGET_INSTANCE_ID
+  wait_available $TARGET_INSTANCE_ID
+}
+
+wait_available() {
+	INST_ID=${1}
+	SLEEP_TIME=10
+	STATUS=stopped
+	while true; do
+		current_status=`aws ec2 describe-instance-status \
+			--query 'InstanceStatuses[?InstanceId==\`'${INST_ID}'\`]'.InstanceStatus.Status \
+			--output text \
+			--region ${AWS_DEFAULT_REGION}`
+		if [[ "$current_status" = "$STATUS" ]]; then
+			echo -n "."
+		else
+			if [[ "$STATUS" != "stopped" ]]; then echo; fi
+			echo -n "Status ($INST_ID): ${current_status:-stopped}"
+		fi
+		if [[ "$current_status" = "ok" ]]; then
+			echo
+			break
+		fi
+		STATUS=$current_status
+		sleep ${SLEEP_TIME}
+	done
 }
 
 start_instance
--- utils/stop-instance/stop-instance.sh
+++ utils/stop-instance/stop-instance.sh
@@ -6,9 +6,6 @@ stop_instance() {
   # stopping instance
 	echo "Stopping instance $TARGET_INSTANCE_ID"
 	aws ec2 stop-instances --instance-ids $TARGET_INSTANCE_ID
-
-	echo "Waiting for $TARGET_INSTANCE_ID to be available"
-  aws ec2 instance-stopped --instance-ids $TARGET_INSTANCE_ID
 }
 
 stop_instance
