05d8308c72f09b669eddd68cfdd73c9fab8b914c
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-01-28 18:42:08 +0100

    added 3 min sleep to stop instance script

--- utils/stop-instance/stop-instance.sh
+++ utils/stop-instance/stop-instance.sh
@@ -6,6 +6,9 @@ stop_instance() {
   # stopping instance
 	echo "Stopping instance $TARGET_INSTANCE_ID"
 	aws ec2 stop-instances --instance-ids $TARGET_INSTANCE_ID
+
+  # added 3 min sleep for making sure instance goes down before starting it
+  sleep 180
 }
 
 stop_instance
