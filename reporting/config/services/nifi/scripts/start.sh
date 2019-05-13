6e9ba6136a31efb30310ee4ac2dfa311c42b43a1
nateMJ <natemaritim@gmail.com>
2019-05-13 11:28:56 +0300

    Define variables in env file and import it during preloading

--- reporting/config/services/nifi/scripts/start.sh
+++ reporting/config/services/nifi/scripts/start.sh
@@ -2,6 +2,6 @@
 
 cp /config/nifi/libs/* lib/ &&
 /config/nifi/scripts/download-toolkit.sh $1 &&
-/config/nifi/scripts/preload.sh init $1 $2 $3 &&
+/config/nifi/scripts/preload.sh init $1 $2 $3 $4 $5 $6 $7 &&
 cd /opt/nifi/nifi-$1 &&
 ../scripts/start.sh
