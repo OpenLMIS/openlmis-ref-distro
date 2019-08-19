94c20bc4817ca5fe010f6b8adc0ffab5468aedc7
Łukasz Lewczyński <llewczynski@soldevelo.com>
2019-08-19 16:10:29 +0200

    OLMIS-6478: Waiting for consul to be available

--- reporting/config/init.sh
+++ reporting/config/init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "Waiting for consul to be available"
 while ! curl -f "http://consul:8500/v1/agent/self"; do
     sleep 10;
 done
--- reporting/config/services/nginx/init.sh
+++ reporting/config/services/nginx/init.sh
@@ -9,6 +9,11 @@ cp -r /config/nginx/consul-template/* /etc/consul-template/
 echo -n "${NGINX_BASIC_AUTH_USER}:" >> /etc/nginx/.htpasswd
 openssl passwd -apr1 ${NGINX_BASIC_AUTH_PW} >> /etc/nginx/.htpasswd
 
-sleep 120
+echo "Waiting for consul to be available"
+while ! curl -f "http://consul:8500/v1/agent/self"; do
+    sleep 10;
+done
+
+sleep 60;
 
 /home/run.sh
