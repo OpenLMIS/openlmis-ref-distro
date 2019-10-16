22635d6048e89eaf967a12eaebabc5cb65926569
Daniel Serkowski <dserkowski@soldevelo.com>
2019-10-16 09:36:37 +0200

    OLMIS-6589: Added Superset UI compilation logic

--- reporting/config/services/superset/init.sh
+++ reporting/config/services/superset/init.sh
@@ -4,6 +4,8 @@ set -e
 
 : ${SUPERSET_ADMIN_USERNAME:?"Need to set SUPERSET_ADMIN_USERNAME"}
 : ${SUPERSET_ADMIN_PASSWORD:?"Need to set SUPERSET_ADMIN_PASSWORD"}
+: ${SUPERSET_VERSION:?"Need to set SUPERSET_VERSION"}
+: ${APP_DIR:?"Need to set APP_DIR"}
 
 CONFIG_DIR="/etc/superset"
 
@@ -22,11 +24,11 @@ done
 >&2 echo "Postgres is up"
 
 # App initialization
-fabmanager create-admin --app superset --username ${SUPERSET_ADMIN_USERNAME} --firstname Admin --lastname Admin --email noreply --password ${SUPERSET_ADMIN_PASSWORD}
+fabmanager create-admin --app superset --username ${SUPERSET_ADMIN_USERNAME} --firstname Admin --lastname Admin --email noreply --password ${SUPERSET_ADMIN_PASSWORD} &&
 
-superset db upgrade
-superset import_datasources -p $CONFIG_DIR/datasources/database.yaml
-superset import_dashboards -p $CONFIG_DIR/dashboards/openlmis_uat_dashboards.json
-superset init
+superset db upgrade &&
+superset import_datasources -p $CONFIG_DIR/datasources/database.yaml &&
+superset import_dashboards -p $CONFIG_DIR/dashboards/openlmis_uat_dashboards.json &&
+superset init &&
 
 gunicorn -w 2 --timeout 60 -b 0.0.0.0:8088 --reload --limit-request-line 0 --limit-request-field_size 0 superset:app
