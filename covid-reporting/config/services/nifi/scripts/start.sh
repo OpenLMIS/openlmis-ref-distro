8c0668bf65ee26ce969bd37c1452eb5e9456c1fa
aswiszcz <aswiszcz@soldevelo.com>
2020-05-29 14:41:53 +0200

    COV-75 - Moving covid-reporting to covid repository

--- covid-reporting/config/services/nifi/scripts/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/config/nifi/scripts/hideUsernameWithinNifi.sh $1 &
-
-cp /config/nifi/libs/* lib/ &&
-/config/nifi/scripts/download-toolkit.sh $1 &&
-/config/nifi/scripts/preload.sh init $1 &&
-cd /opt/nifi/nifi-$1 &&
-../scripts/start.sh
--- covid-reporting/db/Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM gliderlabs/alpine:3.4
-
-COPY ./docker-entrypoint-initdb.d /docker-entrypoint-initdb.d
-
-CMD /bin/true
