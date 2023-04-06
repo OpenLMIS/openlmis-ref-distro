efad18c465b242c7604ccae571c3a8a35c4a9b75
sniedzielski <sniedzielski@soldevelo.com>
2023-04-06 12:12:56 +0200

    OLMIS-7642: fixing error with redirect URL after logging into Superset

--- reporting/config/services/superset/superset_config.py
+++ reporting/config/services/superset/superset_config.py
@@ -47,7 +47,8 @@ OAUTH_PROVIDERS = [
             },
             'api_base_url': '%s/api/oauth' % os.environ['OL_BASE_URL'],
             'access_token_url': '%s/api/oauth/token?grant_type=authorization_code' % os.environ['OL_BASE_URL'],
-            'authorize_url': '%s/api/oauth/authorize?' % os.environ['OL_BASE_URL']
+            'authorize_url': '%s/api/oauth/authorize?' % os.environ['OL_BASE_URL'],
+            'custom_redirect_url': '%s/oauth-authorized/openlmis?' % os.environ['SUPERSET_URL'],
         }
     }
 ]
