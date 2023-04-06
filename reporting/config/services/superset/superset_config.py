be9987792e449ea6a269097b43ae66826082bf62
sniedzielski <sniedzielski@soldevelo.com>
2023-04-06 12:14:50 +0200

    OLMIS-7642: removing redundant comma

--- reporting/config/services/superset/superset_config.py
+++ reporting/config/services/superset/superset_config.py
@@ -48,7 +48,7 @@ OAUTH_PROVIDERS = [
             'api_base_url': '%s/api/oauth' % os.environ['OL_BASE_URL'],
             'access_token_url': '%s/api/oauth/token?grant_type=authorization_code' % os.environ['OL_BASE_URL'],
             'authorize_url': '%s/api/oauth/authorize?' % os.environ['OL_BASE_URL'],
-            'custom_redirect_url': '%s/oauth-authorized/openlmis?' % os.environ['SUPERSET_URL'],
+            'custom_redirect_url': '%s/oauth-authorized/openlmis?' % os.environ['SUPERSET_URL']
         }
     }
 ]
