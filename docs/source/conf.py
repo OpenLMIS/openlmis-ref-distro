1861544832256d5f1336c7d37db83bfbbc595866
mgrochalskisoldevelo <mgrochalski@soldevelo.com>
2024-04-19 20:36:10 +0200

    Modify conf.py file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -353,5 +353,5 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
 
-execfile('collect-docs.py')
-execfile('modify-docs.py')
+exec(open('collect-docs.py')).read()
+exec(open('modify-docs.py').read())
