66420c4ccd3b81e7f6feacd4b41306fc0b1ae8b8
lmphatsi <la.mphatsi@gmail.com>
2025-05-17 11:43:48 +0000

    adapt config to use https instead of http

--- reporting/config/services/nginx/init.sh
+++ reporting/config/services/nginx/init.sh
@@ -9,7 +9,14 @@ cp -r /config/nginx/consul-template/* /etc/consul-template/
 echo -n "${NGINX_BASIC_AUTH_USER}:" >> /etc/nginx/.htpasswd
 openssl passwd -apr1 ${NGINX_BASIC_AUTH_PW} >> /etc/nginx/.htpasswd
 
-echo "Waiting for consul to be available"
-sleep 240;
+# echo "Waiting for consul to be available"
+# sleep 240;
+
+echo "Waiting for Consul at consul:8500 to be available…"
+until wget -qO- http://consul:8500/v1/status/leader 2>/dev/null; do
+  printf "."
+  sleep 5
+done
+echo "\nConsul is up—starting Nginx."
 
 /home/run.sh
--- reporting/config/services/superset/superset_config.py
+++ reporting/config/services/superset/superset_config.py
@@ -61,7 +61,7 @@ OAUTH_PROVIDERS = [
 ]
 
 # Set URL scheme for superset-patchup redirect URLs
-PREFERRED_URL_SCHEME = "http"
+PREFERRED_URL_SCHEME = "https"
 
 # The default user self registration role
 AUTH_USER_REGISTRATION_ROLE = "OLMIS_Gamma"
